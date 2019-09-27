244109092a2a148d392419a0f76632aa7f2f9e75
Faviola Caballero <fabycg18@gmail.com>
2019-09-26 18:52:05 -0700

    add js type on sub

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+
   $('#slides').superslides();
+
+  var typed = new Typed(".typed", {
+    strings: ["Web Developer.", "Software Engineer.", "Entrepreneur."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
 });
+
+
+
+// you can add this code if you want the imags to change on their own.
+//   $('#slides').superslides({
+//   animation: 'fade',
+//   play: 5000,
+//   pagination: false
+// });
