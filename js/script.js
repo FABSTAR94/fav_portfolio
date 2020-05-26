4d5635d2379f18c4b7dd5a627aec8dbc57fb2abc
Faviola Caballero <fabycg18@gmail.com>
2020-05-26 15:46:50 -0700

    made alot of changes

--- js/script.js
+++ js/script.js
@@ -1,92 +1,93 @@
+// loader
 $(window).on("load", function() {
 
-    $(".loader .inner").fadeOut(500, function(){
-      $(".loader").fadeOut(750);
+    $(".loader .inner").fadeOut(500, function() {
+        $(".loader").fadeOut(750);
     });
 
-      $(".items").isotope({
-      filter: '*',
-      animationOptions: {
-        duration: 1500,
-        easing:'linear',
-        queue: false
-      }
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 3000,
+            easing: 'linear',
+            queue: false
+        }
     });
 })
 
+// text on banner slides
+$(document).ready(function() {
 
-$(document).ready(function(){
+    $('#slides').superslides();
 
-  $('#slides').superslides();
+    let typed = new Typed(".typed", {
+        strings: ["Web Developer.", "Front End Engineer.", "Front End Developer."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 
-  var typed = new Typed(".typed", {
-    strings: ["Web Developer.", "Software Engineer.", "Entrepreneur."],
-    typeSpeed: 70,
-    loop: true,
-    startDelay: 1000,
-    showCursor: false
-  });
-
-  $('.owl-carousel').owlCarousel({
-      loop:true,
-      items: 4,
-      responsive:{
-          0:{
-            items:1
-          },
-          480:{
-            items:2
-          },
-          768:{
-            items:3
-          },
-          938:{
-            items:4
-          }
-      }
-  });
-// this shows how many you want to be displayed depending on the screen size.
-
-
-  var skillsTopOffset = $(".skillsSection").offset().top;
-  var statsTopOffset = $(".statsSection").offset().top;
-  var countUpFinished = false;
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    });
 
 
-  $(window).scroll(function() {
+    // navbar
+    $("#navigation li a").click(function(e) {
+        e.preventDefault();
+
+        let targetElement = $(this).attr("href");
+        let targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+        // target element is referring to the about, skills, etc
+    });
 
 
-    if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
-      $('.chart').easyPieChart({
-        easing: 'easeInOut',
-        barColor: '#fff',
-        trackColor: false,
-        scaleColor: false,
-        lineWidth: 4,
-        size: 152,
-        onStep: function(from, to, percent) {
-          $(this.el).find('.percent').text(Math.round(percent));
+
+
+    // use const if it will never be reassigned else use let
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        let body = $("body");
+
+        if ($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        } else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+
+
     }
-  });
+
+});
 
 
-
-    }
-     if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-       $(".counter").each(function() {
-        var element = $(this);
-        var endVal = parseInt(element.text());
-
-          element.countup(endVal);
-        })
-        countUpFinished = true;
-     }
-
-  });
-
-
-    $("[data-fancybox]").fancybox();
+// owl carousel filters
+ $("[data-fancybox]").fancybox();
 
 
 
@@ -111,42 +112,6 @@ $(this).addClass("current");
 });
 
 
-$("#navigation li a").click(function(e) {
-  e.preventDefault();
-
-  var targetElement = $(this).attr("href");
-  var targetPosition = $(targetElement).offset().top;
-  $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
-  // target element is referring to the about, skills, etc
-});
-
-
-
-
-
-// use const if it will nver be reassigned else use var
-  const nav = $("#navigation");
-  const navTop = nav.offset().top;
-
-    $(window).on("scroll", stickyNavigation);
-
-    function stickyNavigation() {
-
-        var body = $("body");
-
-        if($(window).scrollTop() >= navTop) {
-          body.css("padding-top", nav.outerHeight() + "px");
-          body.addClass("fixedNav");
-  }
-  else {
-    body.css("padding-top", 0);
-    body.removeClass("fixedNav");
-  }
-
-
-  }
-
-});
 
 
 
